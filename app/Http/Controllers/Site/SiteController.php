4cfb35ca649921edb5a3174fb75518a865026066
alex <androsaleksei@gmail.com>
2019-03-23 20:58:04 +0300

    *

--- app/Http/Controllers/Site/SiteController.php
+++ app/Http/Controllers/Site/SiteController.php
@@ -24,33 +24,38 @@ class SiteController extends Controller
 
     public function dishes_viewed_save($id)
     {
-        $dishes_cookie = request()->cookie('dishes_viewed') ? explode(',',request()->cookie('dishes_viewed')) : [];
+        $dishes_cookie =
+            request()->cookie('dishes_viewed')
+            ? explode(',', request()->cookie('dishes_viewed'))
+            : [];
 
-        if(!in_array($id, $dishes_cookie)){
+        if (!in_array($id, $dishes_cookie)) {
+            if(count($dishes_cookie) == 3){
+                $dishes_cookie = array_slice($dishes_cookie, 0, 2);
+            }
             $dishes_cookie[] = $id;
-            $dishes_cookie = array_slice($dishes_cookie, 0, 10);
         }
 
         $dishes_viewed = [];
-        if($dishes_cookie){
+        if ($dishes_cookie) {
             $tofind = [];
-            foreach ($dishes_cookie as $_id){
-                if($_id == $id) continue;
+            foreach ($dishes_cookie as $_id) {
+                if ($_id == $id) continue;
                 $tofind[] = $_id;
             }
-            foreach (Dish::find($tofind) as $dish){
+            foreach (Dish::find($tofind) as $dish) {
                 $dishes_viewed[] = [
                     'id' => $dish->id,
                     'name' => $dish->name,
                     'short_description' => $dish->short_description,
                     'price' => $dish->price,
                     'new_price' => $dish->new_price,
-                    'img' => \Storage::disk('public')->exists('dish_imgs/'.$dish->id.'/img_xs.jpg') ? \Storage::disk('public')->url('dish_imgs/'.$dish->id.'/img_xs.jpg') : null,
+                    'img' => \Storage::disk('public')->exists('dish_imgs/' . $dish->id . '/img_xs.jpg') ? \Storage::disk('public')->url('dish_imgs/' . $dish->id . '/img_xs.jpg') : null,
                 ];
             }
-            return response()->json(['dishes_viewed'=>$dishes_viewed])->withCookie(cookie('dishes_viewed', implode(',', $dishes_cookie), 3600));
-        }else{
-            return response()->json(['dishes_viewed'=>$dishes_viewed]);
+            return response()->json(['dishes_viewed' => $dishes_viewed])->withCookie(cookie('dishes_viewed', implode(',', $dishes_cookie), 3600));
+        } else {
+            return response()->json(['dishes_viewed' => $dishes_viewed]);
         }
 
     }
