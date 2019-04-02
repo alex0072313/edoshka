e4d13bc8888e6c5cdb9183a6741fa30f9ac7c8df
alex <androsaleksei@gmail.com>
2019-04-02 10:36:12 +0300

    *

--- resources/views/admin/dishes/form.blade.php
+++ resources/views/admin/dishes/form.blade.php
@@ -327,7 +327,7 @@
                 dataType: 'json',
                 success: function (json) {
                     //if(typeof json.repeat !== "undefined"){
-                        if(json.repeat.name){
+                        if(typeof son.repeat.name !== 'undefined'){
                             if(!input.next('.has_repeat').length){
                                 input.after('<span class="invalid-feedback text-orange has_repeat d-block" role="alert">'+json.repeat.name+'</span>');
                             }
