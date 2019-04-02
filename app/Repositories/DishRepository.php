29a0e22753e09d1fe9e6a877faf976a814bd22ab
alex <androsaleksei@gmail.com>
2019-04-02 07:51:16 +0300

    *

--- app/Repositories/DishRepository.php
+++ app/Repositories/DishRepository.php
@@ -24,31 +24,31 @@ abstract class DishRepository extends Repository
 //        $img->fit(539, 512, function ($constraint) {
 //            $constraint->upsize();
 //        });
-        $img->resizeCanvas(539, 512);
+        $img->crop(539, 512);
         Storage::disk('public')->put('dish_imgs/'.$dish->id.'/img_l.jpg', (string) $img->encode());
 
 //        $img->fit(236, 228, function ($constraint) {
 //            $constraint->upsize();
 //        });
-        $img->resizeCanvas(236, 228);
+        $img->crop(236, 228);
         Storage::disk('public')->put('dish_imgs/'.$dish->id.'/img_m.jpg', (string) $img->encode());
 
 //        $img->fit(120, 120, function ($constraint) {
 //            $constraint->upsize();
 //        });
-        $img->resizeCanvas(120, 120);
+        $img->crop(120, 120);
         Storage::disk('public')->put('dish_imgs/'.$dish->id.'/img_s.jpg', (string) $img->encode());
 
 //        $img->fit(60, 60, function ($constraint) {
 //            $constraint->upsize();
 //        });
-        $img->resizeCanvas(60, 60);
+        $img->crop(60, 60);
         Storage::disk('public')->put('dish_imgs/'.$dish->id.'/img_xs.jpg', (string) $img->encode());
 
 //        $img->fit(36, 36, function ($constraint) {
 //            $constraint->upsize();
 //        });
-        $img->resizeCanvas(36, 36);
+        $img->crop(36, 36);
         Storage::disk('public')->put('dish_imgs/'.$dish->id.'/img_xxs.jpg', (string) $img->encode());
 
     }
