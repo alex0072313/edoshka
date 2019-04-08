1b36878bdb4304d45ac1b2ec9e1d823aaac9def3
alex <androsaleksei@gmail.com>
2019-04-08 10:35:17 +0300

    *

--- resources/views/layouts/site.blade.php
+++ resources/views/layouts/site.blade.php
@@ -6,6 +6,8 @@
 		  content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
 	<meta http-equiv="X-UA-Compatible" content="ie=edge">
 
+    <meta name="yandex-verification" content="0a1e277eb4e3ec2a" />
+
 	<link href="/images/theme/fav.png" rel="icon" />
 	<meta name="csrf-token" content="{{ csrf_token() }}">
 	<link rel="stylesheet" href="{{ asset('css/app.css') }}">
