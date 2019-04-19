aa91a188959719cfb764562f05ec6198eda36619
alex <androsaleksei@gmail.com>
2019-04-19 11:07:16 +0300

    *

--- app/Http/Controllers/Admin/HomeController.php
+++ app/Http/Controllers/Admin/HomeController.php
@@ -14,8 +14,7 @@ class HomeController extends AdminController
         if(\Auth::user()->hasRole('megaroot')){
             $orders = Order::all()->sortByDesc('created_ad');
         }else{
-
-            $orders = \Auth::user()->restaurant()->orders()->sortByDesc('created_ad')->gey();
+            $orders = \Auth::user()->restaurant->orders->sortByDesc('created_ad');
         }
 
         $orders = $orders->map(function ($order){
