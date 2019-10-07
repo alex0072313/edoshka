6de81db7803353ebad7d3beeba2e3d6a9b928499
Alex Andros <androsaleksei@gmail.com>
2019-10-07 11:24:14 +0300

    session

--- config/session.php
+++ config/session.php
@@ -153,7 +153,7 @@ return [
     |
     */
 
-    'domain' => env('SESSION_DOMAIN', null),
+    'domain' => env('.'.env('APP_DOMAIN'), null),
 
     /*
     |--------------------------------------------------------------------------
