657b627c57c9fc5fc2ddff4bea7ef844c292d4a3
Alex Andros <androsaleksei@gmail.com>
2019-10-07 10:46:26 +0300

    subdomain

--- app/Http/Middleware/CheckSubdomain.php
+++ app/Http/Middleware/CheckSubdomain.php
@@ -45,39 +45,5 @@ class CheckSubdomain
 
         return $abort ? abort(404) : $next($request);
 
-//        $stop = false;
-//        //Проверяем город
-//        if(count($domain = explode('.', request()->getHost())) > 2){
-//            $stop = true;
-//            if($town = Town::where('alias', '=', $domain[0])->first()){
-//
-//                switch (request()->route()->getName()){
-//
-//                    case 'site.home':
-//                        $stop = false;
-//                    break;
-//
-//                    case 'site.category':
-//                        $stop = false;
-//                    break;
-//
-//                    case 'site.restaurant':
-//                        if($restaurant = request()->route('restaurant_alias')){
-//                            if($restaurant->town_id == $town->id){
-//                                $stop = false;
-//                            }else{
-//                                return abort(404);
-//                            }
-//                        }
-//                    break;
-//
-//                }
-//
-//            }
-//        }
-//
-//
-//        return $stop ? redirect(\Config::get('app.url')) : $next($request);
-
     }
 }
