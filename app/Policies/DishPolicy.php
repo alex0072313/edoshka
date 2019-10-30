1498e982902426c190c51066d5e22762d1906ade
alex <androsaleksei@gmail.com>
2019-10-30 19:33:59 +0300

    *

--- app/Policies/DishPolicy.php
+++ app/Policies/DishPolicy.php
@@ -22,7 +22,12 @@ class DishPolicy
         // TODO: проверка прав доступа к блюду
         if($user->hasRole('megaroot')){
             return true;
+        }elseif ($user->hasRole('root')){
+            if($dish->restaurant->present_id == $user->id){
+                return true;
+            }
         }
+
         return ($dish->restaurant->id == $user->restaurant->id);
     }
 
