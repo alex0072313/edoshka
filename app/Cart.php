937d9420ad49fab936d06e443cb9007ce1f55ecb
aleksejandros <androsaleksei@gmail.com>
2020-04-02 19:52:51 +0300

    cart

--- app/Cart.php
+++ app/Cart.php
@@ -7,6 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class Cart extends Model
 {
     protected $fillable = ['chat_id', 'dish_id'];
+    public $timestamps = false;
 
     public function dish()
     {
