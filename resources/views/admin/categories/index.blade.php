5ed139d251797b76df2f3814433a87ebcdab648b
aleksejandros <androsaleksei@gmail.com>
2020-04-10 15:15:48 +0300

    fix cats admin

--- resources/views/admin/categories/index.blade.php
+++ resources/views/admin/categories/index.blade.php
@@ -63,62 +63,62 @@
                 </thead>
                 <tbody id="sort_items">
                 @foreach($categories as $category)
-                    <tr data-id="{{$category->id}}">
-                        <td class="pr-0">
-                            {{ $category->id }}
-                        </td>
-                        <td width="1%" class="with-img">
-                            @if(isset($category->id) && Storage::disk('public')->exists('category_imgs/'.$category->id.'/img_xs.jpg'))
-                                <img src="{{ Storage::disk('public')->url('category_imgs/'.$category->id.'/img_xs.jpg') }}" class="img-rounded rounded-circle" />
-                            @endif
-                        </td>
-                        <td>
-                            @if(Auth::user()->can('access', $category))
-                                <a href="{{ route('admin.categories.edit', $category->id) }}">
-                            @endif
-                                {{ $category->name }}
-                            @if(Auth::user()->can('access', $category))
-                                </a>
-                            @endif
-                        </td>
-                        <td>
-                            @php
-                                $rest = $category->restaurant();
-                            @endphp
-                            @if(!is_null($rest))
-                                @if(Auth::user()->can('access', $rest))
-                                    <a href="{{ route('admin.restaurants.edit', $rest->id) }}">
-                                @endif
-                                    {{ isset($rest->name) ? $rest->name : ' - ' }}
-                                @if(Auth::user()->can('access', $rest))
-                                    </a>
-                                @endif
-                            @else
-                                Для всех
-                            @endif
-                        </td>
-                        <td>
-                            {{ $category->alias }}
-                        </td>
-                        <td>{{ $category->topmenu ? 'Да' : 'Нет' }}</td>
-                        @role('admin')
-                            <td><a class="text-green" href="{{ route('user.edit', $category->user->id) }}">{{ $category->user->name }}</a></td>
-                        @endrole
-                        <td class="with-btn text-center" nowrap>
+{{--                    <tr data-id="{{$category->id}}">--}}
+{{--                        <td class="pr-0">--}}
+{{--                            {{ $category->id }}--}}
+{{--                        </td>--}}
+{{--                        <td width="1%" class="with-img">--}}
+{{--                            @if(isset($category->id) && Storage::disk('public')->exists('category_imgs/'.$category->id.'/img_xs.jpg'))--}}
+{{--                                <img src="{{ Storage::disk('public')->url('category_imgs/'.$category->id.'/img_xs.jpg') }}" class="img-rounded rounded-circle" />--}}
+{{--                            @endif--}}
+{{--                        </td>--}}
+{{--                        <td>--}}
+{{--                            @if(Auth::user()->can('access', $category))--}}
+{{--                                <a href="{{ route('admin.categories.edit', $category->id) }}">--}}
+{{--                            @endif--}}
+{{--                                {{ $category->name }}--}}
+{{--                            @if(Auth::user()->can('access', $category))--}}
+{{--                                </a>--}}
+{{--                            @endif--}}
+{{--                        </td>--}}
+{{--                        <td>--}}
+{{--                            @php--}}
+{{--                                $rest = $category->restaurant();--}}
+{{--                            @endphp--}}
+{{--                            @if(!is_null($rest))--}}
+{{--                                @if(Auth::user()->can('access', $rest))--}}
+{{--                                    <a href="{{ route('admin.restaurants.edit', $rest->id) }}">--}}
+{{--                                @endif--}}
+{{--                                    {{ isset($rest->name) ? $rest->name : ' - ' }}--}}
+{{--                                @if(Auth::user()->can('access', $rest))--}}
+{{--                                    </a>--}}
+{{--                                @endif--}}
+{{--                            @else--}}
+{{--                                Для всех--}}
+{{--                            @endif--}}
+{{--                        </td>--}}
+{{--                        <td>--}}
+{{--                            {{ $category->alias }}--}}
+{{--                        </td>--}}
+{{--                        <td>{{ $category->topmenu ? 'Да' : 'Нет' }}</td>--}}
+{{--                        @role('admin')--}}
+{{--                            <td><a class="text-green" href="{{ route('user.edit', $category->user->id) }}">{{ $category->user->name }}</a></td>--}}
+{{--                        @endrole--}}
+{{--                        <td class="with-btn text-center" nowrap>--}}
 {{--                            @if(Auth::user()->can('access', $category))--}}
 {{--                                <a href="{{ route('admin.categories.edit', $category->id) }}" class="btn btn-xs m-r-2 btn-primary"><i class="far fa-xs fa-fw fa-edit"></i></a>--}}
 {{--                                <a href="{{ route('admin.categories.destroy', $category->id) }}" data-click="swal-warning" data-title="Подтвердите действие" data-text="Удалить категорию {{ $category->name }}{{ !is_null($category->dishes()) && $category->dishes()->count() ? ' и ее блюда ('.$category->dishes()->count().')':'' }}?" data-classbtn="danger" data-actionbtn="Удалить" data-type="error" class="btn btn-xs btn-danger"><i class="fas fa-xs fa-fw fa-trash-alt"></i></a>--}}
 {{--                            @else--}}
 {{--                                <span class="label label-secondary">Нет доступа к изменению</span>--}}
 {{--                            @endif--}}
-                        </td>
-                    </tr>
-                    @if($category->childs->count())
-                        @php
-                            $i = 1;
-                        @endphp
-                        @include('admin.includes.category_tree_table_item',['childs' => $category->childs, 'i'=>$i])
-                    @endif
+{{--                        </td>--}}
+{{--                    </tr>--}}
+{{--                    @if($category->childs->count())--}}
+{{--                        @php--}}
+{{--                            $i = 1;--}}
+{{--                        @endphp--}}
+{{--                        @include('admin.includes.category_tree_table_item',['childs' => $category->childs, 'i'=>$i])--}}
+{{--                    @endif--}}
                 @endforeach
                 </tbody>
             </table>
