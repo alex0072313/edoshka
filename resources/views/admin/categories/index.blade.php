39aa70d26d93cb744c79b555169bcecbb42f6ce1
aleksejandros <androsaleksei@gmail.com>
2020-04-10 15:15:03 +0300

    fix cats admin

--- resources/views/admin/categories/index.blade.php
+++ resources/views/admin/categories/index.blade.php
@@ -105,12 +105,12 @@
                             <td><a class="text-green" href="{{ route('user.edit', $category->user->id) }}">{{ $category->user->name }}</a></td>
                         @endrole
                         <td class="with-btn text-center" nowrap>
-                            @if(Auth::user()->can('access', $category))
-                                <a href="{{ route('admin.categories.edit', $category->id) }}" class="btn btn-xs m-r-2 btn-primary"><i class="far fa-xs fa-fw fa-edit"></i></a>
-                                <a href="{{ route('admin.categories.destroy', $category->id) }}" data-click="swal-warning" data-title="Подтвердите действие" data-text="Удалить категорию {{ $category->name }}{{ !is_null($category->dishes()) && $category->dishes()->count() ? ' и ее блюда ('.$category->dishes()->count().')':'' }}?" data-classbtn="danger" data-actionbtn="Удалить" data-type="error" class="btn btn-xs btn-danger"><i class="fas fa-xs fa-fw fa-trash-alt"></i></a>
-                            @else
-                                <span class="label label-secondary">Нет доступа к изменению</span>
-                            @endif
+{{--                            @if(Auth::user()->can('access', $category))--}}
+{{--                                <a href="{{ route('admin.categories.edit', $category->id) }}" class="btn btn-xs m-r-2 btn-primary"><i class="far fa-xs fa-fw fa-edit"></i></a>--}}
+{{--                                <a href="{{ route('admin.categories.destroy', $category->id) }}" data-click="swal-warning" data-title="Подтвердите действие" data-text="Удалить категорию {{ $category->name }}{{ !is_null($category->dishes()) && $category->dishes()->count() ? ' и ее блюда ('.$category->dishes()->count().')':'' }}?" data-classbtn="danger" data-actionbtn="Удалить" data-type="error" class="btn btn-xs btn-danger"><i class="fas fa-xs fa-fw fa-trash-alt"></i></a>--}}
+{{--                            @else--}}
+{{--                                <span class="label label-secondary">Нет доступа к изменению</span>--}}
+{{--                            @endif--}}
                         </td>
                     </tr>
                     @if($category->childs->count())
